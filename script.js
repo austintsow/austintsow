5dece6e61650d07ddcf9098f0563d156f344ec08
Austin Tsow <atsow@icloud.com>
2023-11-18 16:56:19 -0800

    new font and implementing css

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "150px";
-    document.getElementById("main").style.marginLeft = "150px";
+  document.getElementById("mySidenav").style.width = "150px";
+  document.getElementById("main").style.marginLeft = "150px";
+}
+  
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+
+let elements = document.querySelectorAll('.rolling-text');
+
+elements.forEach(element => {
+  let innerText = element.innerText;
+  element.innerHTML = '';
+  
+  let textContainer = document.createElement('div');
+  textContainer.classList.add('block');
+  
+  for (let letter of innerText) {
+    let span = document.createElement('span');
+    span.innerText = letter.trim() === '' ? '\xa0': letter;
+    span.classList.add('letter');
+    textContainer.appendChild(span);
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  element.appendChild(textContainer);
+  element.appendChild(textContainer.cloneNode(true));
+});
+
+// for presentation purpose
+setTimeout(() => {
+  elements.forEach(element => {
+    element.classList.add('play');
+  })
+}, 600);
+
+elements.forEach(element => {
+  element.addEventListener('mouseover', () => {
+    element.classList.remove('play');
+  });
+});
